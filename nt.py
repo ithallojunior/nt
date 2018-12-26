9a385c20d65aed0dda85713f29d54c69e6db126a
ithallojunior <ithallojunior@outlook.com>
2018-12-26 00:04:56 -0200

    adding colors, correcting stuff, etc.

--- nt.py
+++ nt.py
@@ -4,20 +4,20 @@
 author:  Ithallo J. A. Guimarães
 Note taking app
 """
-import commands,tty, sys, termios, os
+import commands, tty, sys, termios, os
 from subprocess import call
-
+import colorama #colors
+import time
 class notes:
 
     def __init__(self): 
-        f = open(".notesrc", "r")
-        configs = eval(f.read())
-        f.close()
+        #either edit it or use the setup
+        self.notes_path = "PATH" + ".notes/"
+        self.default_editor = "EDITOR"
+        self.marker = colorama.Back.MARKER
+        self.no_marker = colorama.Style.RESET_ALL
+
         self.file_to_edit = ""  
-        self.notes_path = configs["NOTES_PATH"]
-        self.default_editor = configs["EDITOR"]
-        self.marker = configs["MARKER"] 
-        self.reverse_marker = configs["REVERSE_MARKER"] 
         self.cmd = ""
         self._position = 0
         self.files = []
@@ -33,8 +33,8 @@ class notes:
         self.cmd = ch
 
     def _get_all_files(self): 
-        self.files = commands.getoutput(" ls %s"%self.notes_path).split()
-    
+        #self.files = commands.getoutput(" ls %s"%self.notes_path).split()
+        self.files = os.listdir(self.notes_path)
     def _clear(self):
         os.system(" clear")
 
@@ -60,9 +60,10 @@ class notes:
                     print("\nDelete %s? [y/N]"%self.files[self._position])
                     self._getchar()
                     if (self.cmd == "y"):
-                        os.system(" rm %s%s"%(self.notes_path, self.files[self._position]) )
+                        #os.system(" rm %s%s"%(self.notes_path, self.files[self._position]) )
+                        os.remove(self.notes_path + self.files[self._position])
                         print("\n%s deleted!"%self.files[self._position])
-                        os.system("sleep 1")
+                        time.sleep(1)
                         self._position = 0
                 #open
                 elif((self.cmd == "e") or (self.cmd == "l") ):
@@ -83,39 +84,44 @@ class notes:
         
         #check for file path existence
         if (commands.getstatusoutput(" ls %s"%self.notes_path)[0] != 0):
-            commands.getoutput(" mkdir %s"%self.notes_path)
+            #commands.getoutput(" mkdir %s"%self.notes_path)
+            os.mkdir(self.notes_path)
             print("Note path created")
         else:
             pass
         
         while(1):
             self._get_all_files()
-            print("__________________________________________________________________")
-            print("|N - new | D - delete | e/l - edit | q - quit | j - down | k - up|")
-            print("|--------|------------|------------|----------|----------|-------|\n")
+            print(self.no_marker+"__________________________________________________________________")
+            print(self.no_marker+"|N - new | D - delete | e/l - edit | q - quit | j - down | k - up|")
+            print(self.no_marker+"|--------|------------|------------|----------|----------|-------|\n")
             if (self.files!=[]):
                 for i in self.files:
                     try:
                         if (i==self.files[self._position]):
                             mark = self.marker
                         else:
-                            mark = self.reverse_marker
+                            mark = self.no_marker
                     except IndexError:
                         self._position = 0
                     
-                    preview = commands.getoutput(' cat %s%s | awk "NR==1" '%(self.notes_path, i))
+                    
+                    #preview = commands.getoutput(' cat %s%s | awk "NR==1" '%(self.notes_path, i))
+                    with open(self.notes_path + i, "r") as f:
+                        preview = f.readline()
+
                     data = commands.getoutput(" ls -l %s%s"%(self.notes_path, i)).split()[5:8]
                     info = "".join(" " + j for j in data)
                     print("%s %s - %s : %s"%(mark, i, info, preview ) )
             else:
-                print("                   ------No files yet------")
-            print("\n-----------------------------end---------------------------------") 
+                print(self.no_marker+"                   ------No files yet------                    ")
+            print(self.no_marker+"\n-----------------------------end---------------------------------") 
             #### commands
             self._getchar() 
             #to quit
             if (self.cmd == "q"):
                 print("Quitting")
-                os.system(" sleep 0.5")
+                time.sleep(0.5)
                 self._clear()
                 #TODO commit to git here
                 break
